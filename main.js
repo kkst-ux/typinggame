f1d4e222c81a42ae43b6649c8444c2b12b1b4128
kkst-ux <66004174+kkst-ux@users.noreply.github.com>
2020-06-26 00:06:33 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+'use strict';
+
+{
+  const words = ['kusamaru is unko'];
+  let word = words[Math.floor(Math.random() * words.length)];
+  let loc = 0;
+  let score = 0;
+  let miss = 0;
+  const timeLimit = 3 * 1000;
+  let startTime;
+
+  const target = document.getElementById('target');
+  const scoreLabel = document.getElementById('score');
+  const timerLabel = document.getElementById('timer');
+
+
+  function updateTarget() {
+    let placeholder = '';
+    for (let i = 0; i < loc; i++) {
+      placeholder += '_';
+    }
+    target.textContent = placeholder + word.substring(loc);
+  }
+
+  function updateTimer() {
+    const timeLeft = startTime + timeLimit - Date.now();
+    timerLabel.textContent = (timeLeft / 1000).toFixed(2);
+  }
+
+  window.addEventListener('click', () => {
+    // if (e.key === e.spaceKey || e.key === e.enterKey) {
+    //   target.textContent = word;
+    // }
+    target.textContent = word;
+    startTime = Date.now();
+    updateTimer();
+  });
+
+  window.addEventListener('keydown', e => {
+    if (e.key === word[loc]) {
+      loc++;
+      if (loc === word.length) {
+        word = words[Math.floor(Math.random() * words.length)];
+        loc = 0;
+      }
+      updateTarget();
+      score++;
+      scoreLabel.textContent = score;
+    } else {
+      miss++;
+      missLabel.textContent = miss;
+    }
+  });
+}
